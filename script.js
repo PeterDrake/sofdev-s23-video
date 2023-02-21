27380552caf1486b3035025469cfc0d5a54a1f4a
Anders Stall <andersstall@lclark.edu>
2023-02-21 14:27:06 -0800

    im in hell

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-console.log("hello world");
-let obj {
-  name: "hello";
-}
-let jason = "hello world";
-var fs = require("fs");
-fs.writeFile('myjsonfile.json', jason, 'utf8', callback);
+var http = require('http');
+var fs = require('fs');
+http.createServer(function (req, res) {
+  fs.readFile('main.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+}).listen(8080);
 
 function testResults (form) {
     let inputValue = form.inputbox.value;
@@ -20,3 +22,5 @@ function testResults (form) {
     var fs = require("fs");
     fs.writeFile('myjsonfile.json', json, 'utf8', callback);
   }
+
+
