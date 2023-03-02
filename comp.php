cde0c0d8a0f3724d24b44558d37a7bc6aa5ba9be
True Duerden <trueduerden@lclark.edu>
2023-03-02 15:22:02 -0800

    successfully grabs info

--- comp.php
+++ comp.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
 <html lang = "en">
     <head>
-        <title>Video Uploaded</title>
+        <title>Video Compressor - Upload Successful</title>
         <meta charset = "utf-8">
         <link href="compressor.css" type="text/css" rel="stylesheet">
         <link rel="icon" type="image/x-icon" href="favicon.ico">
     </head>
 
     <body>
-        <?php echo '<p>Thank you $_POST["fName"], your video has been successfully uploaded.</p>'; ?>
+        <?php
+        	$firstName = $_POST['f_name'];
+        	echo '<p>Thank you ' . $firstName . ', your video has been successfully uploaded.</p>'; ?>
     </body>
 </html>
