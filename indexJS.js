3aa1a89e77371a8b35e2672a25232a8572aac342
daniel-paterson <drdragon08@hotmail.com>
2023-03-07 13:46:22 -0800

    trying again

---
+++ indexJS.js
@@ -0,0 +1,13 @@
+window.addEventListener("DOMContentLoaded",domLoaded);
+
+function domLoaded(){
+    addEmailValidatior();
+}
+
+function addEmailValidatior(){
+    emailField = document.getElementById("email")
+    emailField.addEventListener("keyup", function (event) {
+    isValidEmail = emailField.checkValidity();
+    console.log(isValidEmail);
+    });
+}
