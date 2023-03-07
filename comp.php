9e942e585f5c1cbda78e2f1f726f05e7ef8d7549
Womzie <lc20-0100@lclark.edu>
2023-03-07 14:53:13 -0800

    added sql stuff

--- comp.php
+++ comp.php
@@ -9,10 +9,27 @@
 
     <body>
         <?php
-        //lolk
+            $con = mysqli_connect("localhost","compression","compression","compression");
             $firstName = $_POST['f_name'];
-            //lol
+            $lastName = $_POST['l_name'];
+            $email = $_POST['e_mail'];
+            $file = $_POST['file'];
+            $size = $_POST['desired_size'];
+            $date = "2000-08-01";
+
+            echo $file; 
+
+            $sql = "INSERT INTO `compressaur` (`FirstName`, `LastName`, `Email`, `FileLocation`, `DesiredSize`, `DueDate`) VALUES ('$firstName', '$lastName', '$email', '/haha/fake', '$size', '$date')";
+            $rs = mysqli_query($con, $sql);
+
         	
-        	echo '<p>Thank you ' . $firstName . ', your video has been successfully uploaded.</p>'; ?>
+        	echo '<p>Thank you ' . $firstName . ', your video has been successfully uploaded.</p>'; 
+            if($rs){
+                echo "COntact Records INSERTED";
+            }
+            else{
+                echo "you suck";
+            }
+            ?>
     </body>
 </html>
