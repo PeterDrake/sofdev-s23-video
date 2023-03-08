ecdd7be4cdfd961699a8168b6d1161718cff1374
Anders Stall <andersstall@lclark.edu>
2023-03-08 11:51:48 -0800

    new

--- comp.php
+++ comp.php
@@ -22,7 +22,7 @@
 
             echo $file; 
 
-            $sql = "INSERT INTO `compressaur` (`FirstName`, `LastName`, `Email`, `FileLocation`, `DesiredSize`, `DueDate`) VALUES ('$firstName', '$lastName', '$email', '/haha/fake', '$size', '$date')";
+            $sql = "INSERT INTO `compressaur` (`FirstName`, `LastName`, `Email`, `File`, `DesiredSize`, `DueDate`) VALUES ('$firstName', '$lastName', '$email', '$file', '$size', '$date')";
             $rs = mysqli_query($con, $sql);
 
         	
