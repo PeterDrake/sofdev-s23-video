67cd254898e13ceae58a536120850e72b0b01546
Anders Stall <andersstall@lclark.edu>
2023-03-09 12:16:37 -0800

    refactor

--- upload.php
+++ upload.php
@@ -1,26 +1,16 @@
+<?php
 
-<!DOCTYPE html>
-<html lang = "en">
-    <head>
-        <title>Video Compressor TEST- Upload Successful</title>
-        <meta charset = "utf-8">
-        <link href="compressor.css" type="text/css" rel="stylesheet">
-        <link rel="icon" type="image/x-icon" href="favicon.ico">
-    </head>
+/* Get the name of the uploaded file */
+$filename = $_FILES['file']['name'];
 
-    <body>
-        <?php 
-            $filename = $_FILES['file']['name'];
-            
-            $location = "upload/".$filename;
+/* Choose where to save the uploaded file */
+$location = "upload/".$filename;
 
-            if(move_uploaded_file($_FILES['file']['tmp_name'], $location)){
-                echo "Success";
-            }
-            else{
-                echo "Failure";
-            }
-        ?>
+/* Save the uploaded file to the local filesystem */
+if ( move_uploaded_file($_FILES['file']['tmp_name'], $location) ) { 
+  echo 'Success'; 
+} else { 
+  echo 'Failure'; 
+}
 
-    </body>
-</html>
+?>
