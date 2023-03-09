0708f53ef87a61de872a5110522c9496779852b6
Anders Stall <andersstall@lclark.edu>
2023-03-09 11:48:47 -0800

    minor fix

--- indexJS.js
+++ indexJS.js
@@ -17,4 +17,5 @@ async function saveFile(){
     formData.append("file", fileupload.files[0]);
     await fetch('/upload.php', {method: "POST", body: formData});
     alert('The file has been uploaded successfully.');
+    return 1;
 }
