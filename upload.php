4c9bafd54c734bdb3996787cb9fe97bb0bd3ff48
Anders Stall <andersstall@lclark.edu>
2023-03-09 12:00:21 -0800

    upload test

--- upload.php
+++ upload.php
@@ -1,12 +1,26 @@
-<?php 
-    $filename = $_FILES['file']['name'];
-    
-    $location = "upload/".$filename;
 
-    if(move_uploaded_file($_FILES['file']['tmp_name'], $location)){
-        echo "Success";
-    }
-    else{
-        echo "Failure";
-    }
-?>
+<!DOCTYPE html>
+<html lang = "en">
+    <head>
+        <title>Video Compressor TEST- Upload Successful</title>
+        <meta charset = "utf-8">
+        <link href="compressor.css" type="text/css" rel="stylesheet">
+        <link rel="icon" type="image/x-icon" href="favicon.ico">
+    </head>
+
+    <body>
+        <?php 
+            $filename = $_FILES['file']['name'];
+            
+            $location = "upload/".$filename;
+
+            if(move_uploaded_file($_FILES['file']['tmp_name'], $location)){
+                echo "Success";
+            }
+            else{
+                echo "Failure";
+            }
+        ?>
+
+    </body>
+</html>
