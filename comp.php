005c4d84b48402266effb7d2086ca25ee9e94fa6
Adam Schwarz <aschwarz@lclark.edu>
2023-03-21 13:47:44 -0700

    combine php stuff

--- comp.php
+++ comp.php
@@ -9,39 +9,51 @@
 
     <body>
         <?php
+
+            ini_set('display_errors', 1);
+            ini_set('display_startup_errors', 1);
+            error_reporting(E_ALL);
+
             $con = mysqli_connect("localhost","compression","compression","compression");
             $firstName = $_POST['f_name'];
             $lastName = $_POST['l_name'];
             $email = $_POST['e_mail'];
-            $file = $_POST['file'];
             $size = $_POST['desired_size'];
             $date = $_POST['choose_date'];
 
-            if($date != 'asap') {
-                
-                $date = $_POST['date_due'];
-            }
-            else{
-                
-                $date = date('Y-m-d');
-                var_dump($date);
-                $datetime = new DateTime($date);
-                $datetime->add(new DateInterval('P7D'));
-                $date = $datetime->format('Y-m-d');
-                var_dump($date);
-            }
-            echo $file; 
+            $file = $_FILES['video']['tmp_name'];
+            $filename = $_FILES['video']['name'];
+            $filesize = $_FILES['video']['size'];
+            $upload_dir = 'uploads';
+            $target_file = $upload_dir . DIRECTORY_SEPARATOR . $filename;
 
-            $sql = "INSERT INTO `compressaur` (`FirstName`, `LastName`, `Email`, `FileLocation`, `DesiredSize`, `DueDate`) VALUES ('$firstName', '$lastName', '$email', '/haha/fake', '$size', '$date')";
-            $rs = mysqli_query($con, $sql);
+            if (move_uploaded_file($file, $target_file)) {
+                if($date != 'asap') {
 
-        	
-        	echo '<p>Thank you ' . $firstName . ', your video has been successfully uploaded.</p>'; 
-            if($rs){
-                echo "COntact Records INSERTED";
-            }
-            else{
-                echo "you suck";
+                    $date = $_POST['date_due'];
+                }
+                else{
+
+                    $date = date('Y-m-d');
+                    $datetime = new DateTime($date);
+                    $datetime->add(new DateInterval('P7D'));
+                    $date = $datetime->format('Y-m-d');
+                }
+                echo $file;
+
+                $sql = "INSERT INTO `compressaur` (`FirstName`, `LastName`, `Email`, `FileLocation`, `DesiredSize`, `DueDate`) VALUES ('$firstName', '$lastName', '$email', '/haha/fake', '$size', '$date')";
+                $rs = mysqli_query($con, $sql);
+
+
+                echo '<p>Thank you ' . $firstName . ', your video has been successfully uploaded.</p>';
+                if($rs){
+                    echo "Records INSERTED";
+                }
+                else{
+                    echo "you suck";
+                }
+            } else {
+                echo 'File upload failed.';
             }
             ?>
     </body>
