5e210f7da1a794bbaea590bf1e912eaa77efd2bb
Womzie <lc20-0100@lclark.edu>
2023-04-05 15:40:13 -0700

    select from mysql database using node

--- compress.js
+++ compress.js
@@ -16,14 +16,14 @@ var mysql = require('mysql');
 
 var con = mysql.createConnection({
   host: "localhost",
-  user: "yourusername",
-  password: "yourpassword",
-  database: "mydb"
+  user: "compression",
+  password: "compression",
+  database: "compression"
 });
 
 con.connect(function(err) {
   if (err) throw err;
-  con.query("SELECT * FROM customers", function (err, result, fields) {
+  con.query("SELECT * FROM compressaur", function (err, result, fields) {
     if (err) throw err;
     console.log(result);
   });
