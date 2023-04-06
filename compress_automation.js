d663d7b3c19edc9538e2e7391d79404a46a5627c
Adam Schwarz <aschwarz@lclark.edu>
2023-04-06 15:22:14 -0700

    read all files and pass to different function to do everything else

--- compress_automation.js
+++ compress_automation.js
@@ -4,21 +4,22 @@ const fs = require('fs');
 
 const directoryPath = path.join(__dirname, 'input');
 
-let fileList = [];
+// var x = ( function() {return true;} ) ();
 
-function getListOfFiles(fileList, directoryPath) {
+// function getListOfFiles(directoryPath) {
     fs.readdir(directoryPath, function (err, files) {
 
         if (err) {
             return console.log('Unable to scan directory: ' + err);
+        } else {
+            doEverythingElse(files);
         }
-        files.forEach(file => {
-            console.log(file);
-            fileList.push(file);
-        });
-        return fileList;
     });
+    return -1;
+// }
+
+// console.log(getListOfFiles(directoryPath));
+
+function doEverythingElse(files) {
+    console.log(files);
 }
-
-console.log(getListOfFiles(directoryPath));
-
