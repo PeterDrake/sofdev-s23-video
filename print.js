38e7e617dc947cea221b19602c01e79b770e0fb1
Womzie <lc20-0100@lclark.edu>
2023-04-06 14:29:20 -0700

    pm2 working

--- print.js
+++ print.js
@@ -1,1 +1,8 @@
+//pm2 start print.js --restart-delay=300000 sets 5 minute delay
+// pm2 log
+// pm2 list
+// pm2 delete 0
+
 console.log("hello world");
+setTimeout(function f(){console.log("hello world 2")}, 6000);
+
