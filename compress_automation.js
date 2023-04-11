5a62f13ef4e4f04d6ca79067d7de38a960f0b844
Adam Schwarz <aschwarz@lclark.edu>
2023-04-11 13:05:59 -0700

    connected to sql

--- compress_automation.js
+++ compress_automation.js
@@ -23,4 +23,20 @@ const directoryPath = path.join(__dirname, 'input');
 function doEverythingElse(files) {
     console.log(files);
 
+    var mysql = require('mysql');
+
+    var con = mysql.createConnection({
+        host: "localhost",
+        user: "compression",
+        password: "compression",
+        database: "compression"
+    });
+
+    con.connect(function(err) {
+        if (err) throw err;
+        con.query("SELECT * FROM compressaur", function (err, result, fields) {
+            if (err) throw err;
+            console.log(result);
+        });
+    });
 }
