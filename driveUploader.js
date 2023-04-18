fac61a81a7aa3910e26222a88484a511a72a885d
daniel-paterson <drdragon08@hotmail.com>
2023-04-18 15:26:41 -0700

    updated functions in driveUploaded to return share link

--- driveUploader.js
+++ driveUploader.js
@@ -4,6 +4,7 @@ const {google} = require('googleapis');
 const sendEmail = require('./emailSender.js');
 
 
+
 // service account key file. not on git
 const KEYFILEPATH = './security/auth.json';
 
@@ -21,7 +22,11 @@ async function createAndUploadFile(auth, fileName){
   // init the drive service, it handles calls to the api
   const driveService = google.drive({version:'v3', auth});
 
-  const filePath = ('/output/'+fileName) // path to where the file is stored locally. change as needed
+  // path to where the file is stored locally. change as needed
+  const filePath = ('/output/'+fileName)
+
+  // link to view and download file. updated with value if upload works
+  var shareLink = '';
 
   // metadata for the new file on the dive
   let fileMetaData = {
@@ -46,7 +51,7 @@ async function createAndUploadFile(auth, fileName){
   switch(response.status){
     case 200:
       console.log('File created, its share link is: https://drive.google.com/file/d/'+response.data.id+'/view?usp=share_link') // on a success, gives you a shareable link
-
+      shareLink = "https://drive.google.com/file/d/"+response.data.id+"/view?usp=share_link"
       break;
     
     default:
@@ -54,9 +59,12 @@ async function createAndUploadFile(auth, fileName){
       break;
     
   }
+
+  return shareLink;
 }
 
-// call this from other files
+// call this from other files. returns the share link for the uploaded file
 module.exports = function uploadFileToDrive(fileName){
-  createAndUploadFile(auth, fileName);
+  shareLink = createAndUploadFile(auth, fileName);
+  return shareLink;
 }
