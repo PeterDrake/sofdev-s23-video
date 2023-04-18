229107d4ec252e9b11139c0c6bb9aea2ee29b2b6
daniel-paterson <drdragon08@hotmail.com>
2023-04-18 13:23:42 -0700

    got the basic email code to work, now need to make it send email

---
+++ driveUploader.js
@@ -0,0 +1,60 @@
+const fs = require('fs');
+const readline = require('readline');
+const {google} = require('googleapis');
+
+// service account key file. not on git (sorry hackers)
+const KEYFILEPATH = 'auth.json';
+
+// gives full access to google drive account
+const SCOPES = ['https://www.googleapis.com/auth/drive'];
+
+// init the auth thingy with the keyfile and the scope
+const auth = new google.auth.GoogleAuth({
+  keyFile: KEYFILEPATH,
+  scopes: SCOPES
+})
+
+async function createAndUploadFile(auth, fileName){
+
+  // init the drive service, it handles calls to the api
+  const driveService = google.drive({version:'v3', auth});
+
+  const filePath = ('/output/'+fileName) // path to where the file is stored locally. change as needed
+
+  // metadata for the new file on the dive
+  let fileMetaData = {
+    'name': fileName,
+    'parents': ['1gvlj5M577gvG7qU5VQo54xx9fubF0AqN'] //id of the folder these should get put into
+  }
+
+  // making the file to upload
+  let media = {
+    mimeType: 'video/mp4',
+    body: fs.createReadStream(filePath)
+  }
+
+  // make a request
+  let response = await driveService.files.create({
+    resource: fileMetaData,
+    media: media,
+    fields: 'id'
+  })
+
+  // handle the response
+  switch(response.status){
+    case 200:
+      console.log('File created, its share link is: https://drive.google.com/file/d/'+response.data.id+'/view?usp=share_link') // on a success, gives you a shareable link
+
+      break;
+    
+    default:
+      console.error('Error creating files, ' + response.errors)
+      break;
+    
+  }
+}
+
+// call this from other files
+function uploadFileToDrive(fileName){
+  createAndUploadFile(auth, fileName);
+}
