dcdc7234c8f996a4e9936fa3f8199e1d4a7c0766
True Duerden <trueduerden@lclark.edu>
2023-04-25 14:18:55 -0700

    scrap progress bar

--- siteJS.js
+++ siteJS.js
@@ -20,28 +20,6 @@ function domLoaded(){
     let currentYear = today.getFullYear();
     let currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
     document.getElementsByName("date_due")[0].setAttribute('min', currentDate);
-
-    const form = document.getElementById('upload-form');
-    const fileInput = document.getElementById('video');
-    const progressBar = document.getElementById('progress');
-
-    form.addEventListener('submit', e => {
-        e.preventDefault();
-
-        const file = fileInput.files[0];
-        const formData = new FormData();
-        formData.append('file', file);
-
-        const xhr = new XMLHttpRequest();
-        xhr.open('POST', '/upload', true);
-        xhr.upload.onprogress = e => {
-            if (e.lengthComputable) {
-                const percentComplete = (e.loaded / e.total) * 100;
-                progressBar.style.width = `${percentComplete}%`;
-            }
-        };
-        //xhr.send(formData);
-    });
 }
 
 function addEmailValidatior(){
